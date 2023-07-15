674a621ca865c436d13bd7a8c05a5e24d45e0f34
Storm <storm_sia_llamas@yahoo.com>
2023-07-15 19:03:24 +0800

    Sass setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import "./assets/scss/main.scss";
+
+const App = () => {
   return (
     <div className="App">
       <header className="section-header"></header>
     </div>
   );
-}
+};
 
 export default App;
